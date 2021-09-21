0e9794f93f940e30cbb0fec68894270761646da8
Joey <joseph.w.woolever@gmail.com>
2021-09-21 12:19:58 -0500

    add/use shared layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,12 @@
 import * as React from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby';
-import { Seo } from '../components/seo.js'
-
+import { Link } from 'gatsby';
+import Layout from '../components/layout.js';
 // pages in gatsby must be a default export to get picked up
 export default function IndexPage() {
-    //use to import the data that currently resides in the gatsby config file 
-  const data = useStaticQuery(graphql`
-    query GetSiteTitle {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  const meta = data?.site?.siteMetadata ?? {};
-
-  
   return (
-    <>
-    <Seo />
-      <header>
-        <Link to="/">{meta.title}</Link>
-      </header>
-      <main>
-        <h1>Hello Friend</h1>
-        <Link to="/about">About</Link>
-      </main>
-    </>
+    <Layout>
+      <h1>Hello Friend</h1>
+      <Link to="/about">About</Link>
+    </Layout>
   );
 }
