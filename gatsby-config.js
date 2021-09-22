cdd4564bac5dbdb1724b2cdeccfa13a70a377f03
Joey <joseph.w.woolever@gmail.com>
2021-09-22 02:13:32 -0500

    setup image querying from graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,26 +2,19 @@ module.exports = {
   siteMetadata: {
     siteUrl: 'https://www.yourdomain.tld',
     title: 'Frontend Masters Intro to Gatsby',
-    description: 'FEM Intro to Gatsby Project',
+    description: 'Frontend Masters Intro to Gatsby course project',
     image:
-      'https://i.rehttps://res.cloudinary.com/dpc3zrcvs/image/upload/c_scale,w_800/v1632173793/ray-brad-sci-fi_qkeujk.jpgd.it/cmtksuaq83671.png',
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-
-    // this set of plugins is to enable creating mdx blog post from the src/post folder
     {
       resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'posts',
-        path: `${__dirname}/src/posts`,
-      },
+      options: { name: 'posts', path: `${__dirname}/src/posts/` },
     },
     {
       resolve: 'gatsby-plugin-page-creator',
-      options: {
-        path: `${__dirname}/src/posts`,
-      },
+      options: { path: `${__dirname}/src/posts/` },
     },
     'gatsby-remark-images',
     {
@@ -40,13 +33,9 @@ module.exports = {
         },
       },
     },
-    //end mdx config
     {
       resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
+      options: { name: 'images', path: `${__dirname}/src/images/` },
     },
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
