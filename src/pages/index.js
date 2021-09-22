7594e29d2646db7f7038c14258c9fef944a4038e
Joey <joseph.w.woolever@gmail.com>
2021-09-21 20:31:27 -0500

    added blog roll to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,42 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout.js';
 // pages in gatsby must be a default export to get picked up
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            title
+            description
+            date(fromNow: true)
+          }
+        }
+      }
+    }
+  `);
+
+  const posts = data.allMdx.nodes;
+
   return (
     <Layout>
       <h1>Hello Friend</h1>
       <Link to="/about">About</Link>
+
+      <h2>Check out recent blog posts</h2>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+              <small>posted {post.frontmatter.date}</small>
+            </li>
+          );
+        })}
+      </ul>
     </Layout>
   );
 }
